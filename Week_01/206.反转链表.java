b450d9a855dca7856d571b73698b2e884d01f8c5
ytw <yqqq2015@gmail.com>
2020-09-13 14:18:06 +0800

    feat: 70 爬楼梯，206 翻转链表

---
+++ Week_01/206.反转链表.java
@@ -0,0 +1,46 @@
+import org.graalvm.compiler.code.HexCodeFile;
+
+/*
+ * @lc app=leetcode.cn id=206 lang=java
+ *
+ * [206] 反转链表
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (70.48%)
+ * Likes:    1217
+ * Dislikes: 0
+ * Total Accepted:    329.9K
+ * Total Submissions: 467.9K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 反转一个单链表。
+ * 
+ * 示例:
+ * 
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+}
+// @lc code=end
---
+++ Week_01/70.爬楼梯.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=java
+ *
+ * [70] 爬楼梯
+ *
+ * https://leetcode-cn.com/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (50.52%)
+ * Likes:    1223
+ * Dislikes: 0
+ * Total Accepted:    281K
+ * Total Submissions: 556.1K
+ * Testcase Example:  '2'
+ *
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 
+ * 注意：给定 n 是一个正整数。
+ * 
+ * 示例 1：
+ * 
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 
+ * 示例 2：
+ * 
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int climbStairs(int n) {
+        int n1 = 0, n2 = 1, n3 = 2;
+        for (int i = n3; i < n + 1; i++) {
+            n1 = n2;
+            n2 = n3;
+            n3 = n1 + n2;
+        }
+        return n2;
+    }
+}
+// @lc code=end
