e39ebae13ffef3f6d2bc1a504bb5681e9be667c6
ytw <yqqq2015@gmail.com>
2020-09-20 15:19:54 +0800

    feat: 429 N叉树的层序遍历

---
+++ Week_02/429.n叉树的层序遍历.java
@@ -0,0 +1,93 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import jdk.internal.jimage.ImageReader.Node;
+
+/*
+ * @lc app=leetcode.cn id=429 lang=java
+ *
+ * [429] N叉树的层序遍历
+ *
+ * https://leetcode-cn.com/problems/n-ary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (66.40%)
+ * Likes:    108
+ * Dislikes: 0
+ * Total Accepted:    29.2K
+ * Total Submissions: 43.9K
+ * Testcase Example:  '[1,null,3,2,4,null,5,6]'
+ *
+ * 给定一个 N 叉树，返回其节点值的层序遍历。 (即从左到右，逐层遍历)。
+ * 
+ * 例如，给定一个 3叉树 :
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 返回其层序遍历:
+ * 
+ * [
+ * ⁠    [1],
+ * ⁠    [3,2,4],
+ * ⁠    [5,6]
+ * ]
+ * 
+ * 
+ * 
+ * 
+ * 说明:
+ * 
+ * 
+ * 树的深度不会超过 1000。
+ * 树的节点总数不会超过 5000。
+ * 
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<Node> queue = new LinkedList<>();
+        if (root == null)
+            return res;
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node last = queue.poll();
+                level.add(last.val);
+                queue.addAll(last.children);
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
+// @lc code=end
